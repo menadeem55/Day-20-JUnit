5ae673e107222853558952c1be400fdfea94a324
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-30 14:46:35 +0530

    [Added: Testing of Method LastName]

--- src/com/junit/java/userRegistration.java
+++ src/com/junit/java/userRegistration.java
@@ -8,12 +8,20 @@ import org.junit.Test;
 
 public class userRegistration {
 	
-	
+	@Test
 	public boolean firstName(String firstName) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
 		return matcher.matches();
+	}
+	@Test
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+		
 	}
 	
 }
--- src/com/junit/java/userRegistrationTest.java
+++ src/com/junit/java/userRegistrationTest.java
@@ -7,17 +7,29 @@ import junit.framework.Assert;
 public class userRegistrationTest {
 
 	userRegistration userRegistration = new userRegistration();
-	 
 
-    @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Shadab");
-        Assert.assertEquals(true, result);
-    }
-    @Test
-	    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-        boolean result = userRegistration.firstName("shadab");
-        Assert.assertEquals(false, result);
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Nadeem");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("nadeem");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Akhtar");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("akhtar");
+		Assert.assertEquals(false, result);
 	}
 
 }
