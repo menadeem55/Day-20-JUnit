6d997e7de4591dfd918c2ebd8495d705f99dde7d
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-02 13:22:44 +0530

    [Added: JUnit Parametrised Test]

--- src/com/junit/java/userRegistration.java
+++ src/com/junit/java/userRegistration.java
@@ -1,38 +1,14 @@
 package com.junit.java;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.junit.Test;
 
 public class userRegistration {
 
-	private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
-    private static final String EMAIL = "^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$";
-    private static final String PHONE_NUMBER = "^[\\d]{2}[\\s][\\d]{10}$";
-    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=[^!@#$%^&+=]*[!@#$%^&+=][^!@#$%^&+=]*$)(?=.*[0-9]).{8,}$";
 
-    public boolean firstNameValidate(String fName)
-    {
-        return Pattern.matches(NAME, fName);
-    }
-    public boolean lastNameValidate(String lName)
-    {
-        return Pattern.matches(NAME, lName);
-    }
 
-    public boolean emailValidate(String email)
+    public boolean validateEmail(String email)
     {
-        return Pattern.matches(EMAIL, email);
-    }
-
-    public boolean phoneNumberValidate(String phNum)
-    {
-        return Pattern.matches(PHONE_NUMBER, phNum);
-    }
-
-    public boolean passwordValidate(String pw)
-    {
-        return Pattern.matches(PASSWORD, pw);
+    	
+        return Pattern.matches("^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", email);
     }
 }
--- src/com/junit/java/userRegistrationTest.java
+++ src/com/junit/java/userRegistrationTest.java
@@ -1,81 +1,45 @@
 package com.junit.java;
 
-
-import static org.junit.Assert.assertArrayEquals;
-
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import junit.framework.Assert;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.Assert.assertEquals;
+@RunWith(Parameterized.class)
 
 public class userRegistrationTest {
 
-	 @Test
-	    public void givenFirstName_whenProper_ShouldReturnTrue() {
-		 userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.firstNameValidate("Shadab");
-	        Assert.assertTrue(result);
-	    }
+	private String emailTest;
+    private boolean expectedResult;
+    private userRegistration validateEmail;
 
-	    @Test
-	    public void givenFirstName_whenNotProper_ShouldReturnFalse() {
-	    	userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.firstNameValidate("shadab");
-	        Assert.assertFalse(result);
-	    }
+    public userRegistrationTest(String email, boolean expectedResult) {
+        this.emailTest = email;
+        this.expectedResult = expectedResult;
+    }
 
-	    @Test
-	    public void givenLastName_whenProper_ShouldReturnTrue() {
-	    	userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.lastNameValidate("Hussain");
-	        Assert.assertTrue(result);
-	    }
+    @Before
+    public void initialize() {
+        validateEmail = new userRegistration();
+    }
 
-	    @Test
-	    public void givenLastName_whenNotProper_ShouldReturnFalse() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.lastNameValidate("hussain");
-	        Assert.assertFalse(result);
-	    }
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
+                { "abc.100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
+                { "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
+                { "abc+100@gmail.com", true }, { "abc", false }, { "abc@.com.my", false }, { "abc123@gmail.a", false },
+                { "abc123@.com", false }, { "abc123@.com.com", false }, { ".abc@abc.com", false },
+                { "abc()*@gmail.com", false }, { "abc@%*.com", false }, { "abc..2002@gmail.com", false },
+                { "abc.@gmail.com", false }, { "abc@abc@gmail.com", false }, { "abc@gmail.com.1a", false },
+                { "abc@gmail.com.aa.au", false } });
+    }
 
-	    @Test
-	    public void givenEmail_whenProper_ShouldReturnTrue() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.co.in");
-	        Assert.assertTrue(result);
-	    }
-
-	    @Test
-	    public void givenEmail_whenNotProper_ShouldReturnFalse() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.in");
-	        Assert.assertFalse(result);
-	    }
-
-	    @Test
-	    public void givenPhone_whenProper_ShouldReturnTrue() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.phoneNumberValidate("91 7879219494");
-	        Assert.assertTrue(result);
-	    }
-
-	    @Test
-	    public void givenPhone_whenNotProper_ShouldReturnFalse() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.phoneNumberValidate("7879219494");
-	        Assert.assertFalse(result);
-	    }
-
-	    @Test
-	    public void givenPassword_whenProper_ShouldReturnTrue() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.passwordValidate("Abc12345#");
-	        Assert.assertTrue(result);
-	    }
-
-	    @Test
-	    public void givenPassword_whenNotProper_ShouldReturnFalse() {
-	        userRegistration userRegistrationRegEx = new userRegistration();
-	        boolean result = userRegistrationRegEx.passwordValidate("Shadab23#%");
-	        Assert.assertFalse(result);
-	    }
+    @Test
+    public void givenEmailAsVar_ShouldReturnTrueorFalse() {
+        assertEquals(expectedResult, validateEmail.validateEmail(emailTest));
+    }
 }
